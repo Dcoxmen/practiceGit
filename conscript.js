38e10ce386624280357ddc3497cc455a9531a50d
Dave Cox <davedvst@gmail.com>
2021-01-22 13:55:17 -0800

    cleaned up test files

--- conscript.js
+++ conscript.js
@@ -41,7 +41,7 @@ var saveSearch = function(){
 };
 
 var getCityWeather = function(city){
-    var apiKey = "05adf19bd1809fb95f63f7226bd12fb2"
+    var apiKey = "api-key-goes-here"
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
     fetch(apiURL)
